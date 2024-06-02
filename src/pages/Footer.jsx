b72d81a3c8e57ec7de07c84bdcd184203c008680
Yuvi <singhraj0461@gmail.com>
2024-06-02 14:29:27 +0530

    a small bug in footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,6 +7,10 @@ import { SiLeetcode } from "react-icons/si";
 import { FaHackerrank } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { SiGeeksforgeeks } from "react-icons/si";
+import { Link } from "react-router-dom";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div className=" dark:bg-gray-700 bg-[#2f2f2f] text-white p-10  ">
