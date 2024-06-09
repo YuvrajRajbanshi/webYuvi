729e0cb852a488e3baf4a7c245ba0ea00030ca52
Yuvi <singhraj0461@gmail.com>
2024-06-09 15:23:29 +0530

    small bug fixed in the certificate and card file.

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-
 import CertificateCard from "./certificate/CertificateCard";
+import certificate1 from "../../public/photos/certificate.jpg";
+import certificate2 from "../../public/photos/certificate1.jpg";
+import certificate3 from "../../public/photos/certificate2.jpg";
 
 const Certifcate = () => {
   return (
     <div className=" dark:text-white dark:bg-[#2f2f2f] p-10  ">
       <div className=" flex items-center  justify-center flex-row flex-wrap gap-10">
-        <CertificateCard />
-        <CertificateCard />
-        <CertificateCard />
-        <CertificateCard />
-        <CertificateCard />
+        <CertificateCard
+          photos={certificate1}
+          title={"Decode to Code:"}
+          desc={
+            "College competition, our team had secured the first place in this competition"
+          }
+        />
+        <CertificateCard
+          photos={certificate2}
+          title={"GDSC event:"}
+          desc={
+            "In this event we got second place. The event held in our college by CIT Bangalore "
+          }
+        />
+        <CertificateCard
+          photos={certificate3}
+          title={"CIPHER-BATTLE 2.0"}
+          desc={"This hackathon held in CIT Gubbi, Tumakuru."}
+        />
+
+        {/* <CertificateCard />
+        <CertificateCard /> */}
       </div>
     </div>
   );
--- src/pages/certificate/CertificateCard.jsx
+++ src/pages/certificate/CertificateCard.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import certificate1 from "/public/photos/certificate.jpg";
-const CertificateCard = () => {
+import certificate2 from "/public/photos/certificate.jpg";
+import certificate3 from "/public/photos/certificate.jpg";
+
+const CertificateCard = ({ photos, title, desc }) => {
   return (
     <div className=" mt-5">
       <div className=" flex items-center justify-center ">
         <div className=" bg-[#2D343F] text-white h-[350px] w-[350px] p-4  rounded-md">
-          <img src={certificate1} alt="" />
-          <h1 className="font-semibold font-mono text-xl  mt-4">
-            Decode to Code
-          </h1>
+          <img src={photos} alt="" />
+          <h1 className="font-semibold font-mono text-xl  mt-4">{title}</h1>
 
-          <p className=" ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-            architecto.
-          </p>
+          <p className=" ">{desc} </p>
         </div>
       </div>
     </div>
